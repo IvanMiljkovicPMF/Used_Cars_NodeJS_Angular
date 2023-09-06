53b7e3d918c5359349800d55e01624057026dc17
DNovakovic123 <novakovic214146@gmail.com>
2023-09-06 19:45:53 +0200

    first

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContctComponent } from './pages/contct/contct.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CarsAddsComponent } from './pages/cars-adds/cars-adds.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ContctComponent,
+    ContactComponent,
+    RegisterComponent,
+    LoginComponent,
+    CarsAddsComponent
   ],
   imports: [
     BrowserModule
