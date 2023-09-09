2eb6ab744326745271865e39830b114569662506
IvanMiljkovicPMF <ivannmiljkovic00@gmail.com>
2023-09-09 15:16:36 +0200

    filter cars

--- server/services/carsService.js
+++ server/services/carsService.js
@@ -1,9 +1,9 @@
 let CarModel = require('../models/car')
 
 
-let pagination = function(skip,limit){
-    return CarModel.find({}).skip(skip).limit(limit);
-}
+let pagination = function (skip, limit, filter) {
+    return CarModel.find(filter).skip(skip).limit(limit);
+  };
 let getNewCars = function(limit){
     return CarModel.find({Year:{$gt:2020}}).limit(limit);
 }
@@ -14,9 +14,9 @@ let getBestBuyCars = function(limit){
 }
 
 //number of cars in db
-let countAllObj = function()
+let countAllObj = function(filter)
 {
-    return CarModel.countDocuments({});
+    return CarModel.countDocuments(filter);
 }
 
 let getCarByID = function(id)
